caaba1a07c8e2bda2a3379751b6e0d372070f745
root <michael.mk.kaiser@gmail.com>
2021-06-20 16:51:08 +0000

    implemented ETL functionality in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,50 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    """    
+    # load datasets
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)    
+    # merge datasets
+    df = messages.merge(categories, how='inner', on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";",expand=True,)
+    
+    #get category names
+    category_colnames = categories.loc[0, :].str.split(pat="-").str[0]
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    # set each value to be the last character of the string and convert to numeric
+    for column in categories:
+        categories[column] = pd.to_numeric(categories[column].str[-1])
+        
+    # drop the original categories column from `df`
+    df.drop(['categories'], axis=1, inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 
 def save_data(df, database_filename):
+    engine = create_engine("sqlite:///{}".format(database_filename))
+    df.to_sql('disaster_data', engine, index=False)
     pass  
 
 
